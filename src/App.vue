b2ba1de2ecf697ec85d6827a28605fe6edb60c61
Vahe-3 <vahe.avetisyan333@gmail.com>
2024-08-12 16:38:02 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <PopupWithImage :imageUrl="imageUrl" :isOpen="isShowQuest" @close="handleClosePopup">
+      <template v-slot:title>
+        <h1>Какой-то тайтл</h1>
+      </template>
+      <template v-slot:subtitle>
+        <h2>Какой-то сбтайтл</h2>
+      </template>
+      <template v-slot:description>
+        <p>Это описание</p>
+      </template>
+    </PopupWithImage>
+    <button class="open-button" @click="handleOpenPopup">Открыть</button>
+  </div>
+</template>
+
+<script>
+import PopupWithImage from './components/PopupWithImage/PopupWithImage.vue';
+import questionImage from '../public/questionImage.jpg';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      imageUrl: questionImage,
+      isShowQuest: false
+    };
+  },
+  components: {
+    PopupWithImage
+  },
+  methods: {
+    handleOpenPopup() {
+      this.isShowQuest =  true
+    },
+    handleClosePopup() {
+      this.isShowQuest =  false
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  height: 100vh;
+  background-color: antiquewhite;
+}
+
+.open-button {
+  font-size: 20px;
+  margin: 10px;
+}
+
+
+</style>./components/Popup/QuestPopup.vue
---
+++ src/components/PopupWithImage/PopupWithImage.vue
@@ -0,0 +1,58 @@
+<template>
+	<van-popup v-model:show="isOpen" position="bottom" class="popupWithImg">
+		<div class="content">
+			<div class="header">
+				<div class="image-wrapper">
+					<slot v-if="!imageUrl" name="image"></slot>
+					<img v-if="imageUrl" :src="imageUrl" />
+				</div>
+				<div class="close-button"" @click="handleClose">&times;</div>
+			</div>
+			<div class="body">
+				<slot v-if="!title" name="title"></slot>
+				<h1 v-if="title">{{ title }}</h1>
+				<slot v-if="!subtitle" name="subtitle"></slot>
+				<h2 v-if="subtitle"> {{ subtitle }}</h2>
+				<slot v-if="!subtitle" name="description"></slot>
+				<p v-if="subtitle">{{ description }}</p>
+				<slot name="content"></slot>
+			</div>
+		</div>
+	</van-popup>
+</template>
+
+<script>
+import { Popup } from 'vant';
+export default {
+	name: 'PopupWithImage',
+	components: { 'van-popup': Popup },
+
+	props: {
+		imageUrl: {
+			type: String,
+			required: true
+		},
+		title: {
+			type: String,
+		},
+		subtitle: {
+			type: String,
+		},
+		description: {
+			type: String,
+		},
+		isOpen: {
+			type: Boolean,
+			required: true
+		}
+	},
+
+	methods: {
+		handleClose() {
+			this.$emit('close')
+		}
+	}
+}
+</script>
+
+<style scss src="./PopupWithImage.scss"></style>
